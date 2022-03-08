2c3ce6c1b4e52304ff5dcc0694df0179aea06507
Daniel Escobar <dernestoe@yahoo.com>
2022-03-07 21:05:10 -0500

    Agrega paquete y variables de entorno

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,5 @@
 
-export const HOST= "localhost";
-export const USER= "root";
-export const PASSWORD= "";
-export const DB= "testdb"
+export const HOST= process.env.DB_HOST ?? "localhost";
+export const USER= process.env.DB_USER ?? "root";
+export const PASSWORD= process.env.DB_PASS ?? "";
+export const DB= process.env.DB_NAME ?? "testdb"
