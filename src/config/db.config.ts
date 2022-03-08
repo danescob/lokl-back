1126b80b5c18d7239014a194101dfa3d03a683ba
Daniel Escobar <dernestoe@yahoo.com>
2022-03-07 22:54:02 -0500

    Ajusta la conexion basado en env

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config()
 
 export const HOST= process.env.DB_HOST ?? "localhost";
-export const USER= process.env.DB_USER ?? "root";
+export const USER= process.env.DB_USER ?? "";
 export const PASSWORD= process.env.DB_PASS ?? "";
-export const DB= process.env.DB_NAME ?? "testdb"
+export const DB=process.env.DB_NAME ?? "testdb"
